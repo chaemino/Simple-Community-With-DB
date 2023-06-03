351f074ff08848c15c679463d9bf7455e7d88ac0
chaemino <kachator.u@gmail.com>
2023-06-03 14:04:51 +0900

    test mariaDB connection

--- web/app.js
+++ web/app.js
@@ -8,16 +8,21 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 // mariaDB connect
-const maria = require('../maria')
-maria.connect();
+const maria = require('../maria/maria.js')
+//maria.connect();
 
 app.get('/', (req, res) => {
 	res.send('Hello World')
 });
 
-app.get('/mariaDB', (req, res) ->{
-	maria.query('select * from')
-
+app.get('/mariaDB', (req, res) =>{
+	maria.query('select * from 게시글', (err, rows, fields) => {
+		if (err) throw err;
+		console.log(rows);
+		console.log(fields);
+	});
+	res.send('Connect DB')
+});
 
 app.listen(port, () => {
 	console.log('8000!');
